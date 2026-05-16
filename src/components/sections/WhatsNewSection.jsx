32cf8d899a578029ffd5842fe7ae1fdb3dbcecae
satishdwivedi-01 <dwivedisatish06@gmail.com>
2026-05-16 15:41:21 +0530

    initial

--- src/components/sections/WhatsNewSection.jsx
+++ src/components/sections/WhatsNewSection.jsx
@@ -9,20 +9,20 @@ import selfie from "../../assets/whatsNew/selfie.png";
 
 function WhatsNewSection() {
   return (
-    <section className="relative h-[714px] overflow-hidden">
-      
+    <section className="relative h-[834px] overflow-hidden">
+
       {/* MAIN BG */}
       <div className="absolute inset-0 bg-[linear-gradient(179.84deg,#075BBF_11.36%,#042858_99.86%)]" />
 
       {/* CONTENT */}
       <div className="relative z-20 h-full">
-        
+
         {/* LEFT CONTENT */}
         <div className="absolute left-[54px] top-[66px]">
-          
+
           {/* TITLE */}
           <div className="relative w-[343px] h-[102px]">
-            
+
             {/* TOP */}
             <h2
               className="
@@ -44,7 +44,7 @@ function WhatsNewSection() {
 
             {/* BOTTOM TAG */}
             <div className="absolute left-0 top-[50px] w-[343px] h-[52px]">
-              
+
               <div className="absolute left-0 top-[4px] w-full h-[48px] bg-[#F7C259] skew-x-[-12deg]" />
 
               <h3
@@ -69,7 +69,7 @@ function WhatsNewSection() {
 
           {/* CHARACTER */}
           <div className="relative w-[419px] h-[462px] mt-[58px]">
-            
+
             {/* SHADOW */}
             <div
               className="
@@ -111,7 +111,7 @@ function WhatsNewSection() {
             gap-[18px]
           "
         >
-          
+
           {/* CARD 1 */}
           <div className="relative z-30">
             <WhatsNewCard
@@ -143,53 +143,80 @@ function WhatsNewSection() {
         </div>
       </div>
 
-      {/* BOTTOM CURVED STRIP */}
-      <div
-        className="
-          absolute
-          left-1/2
-          bottom-[-48px]
-          -translate-x-1/2
-          w-[1410px]
-          h-[96px]
-          rounded-[999px]
-          bg-[#FBE1AC]
-          z-30
-        "
-      >
-        
-        {/* TICKER */}
-        <div
-          className="
-            absolute
-            left-0
-            top-[32px]
-            whitespace-nowrap
-            flex
-            items-center
-            gap-5
-            text-[#4A3016]
-            font-['Quicksand']
-            font-semibold
-            text-[18px]
-          "
+      {/* STATIC SNAKE WAVE RIBBON */}
+      <div className="absolute bottom-0 left-0 w-full h-[120px] overflow-hidden z-30">
+
+        {/* MAIN WAVE */}
+        <svg
+          className="absolute inset-0 h-full w-full"
+          viewBox="0 0 1440 120"
+          preserveAspectRatio="none"
         >
-          <span>5 Million+ Happy Visitors</span>
-          <span>•</span>
+          {/* Main Snake Body */}
+          <path
+            d="
+        M0 55
+        C120 5 240 105 360 55
+        C480 5 600 105 720 55
+        C840 5 960 105 1080 55
+        C1200 5 1320 105 1440 55
+        L1440 120
+        L0 120
+        Z
+      "
+            fill="#FBE1AC"
+          />
 
-          <span>5 Million+ Happy Customer</span>
-          <span>•</span>
+          {/* Soft Highlight Layer */}
+          <path
+            d="
+        M0 68
+        C120 25 240 88 360 68
+        C480 25 600 88 720 68
+        C840 25 960 88 1080 68
+        C1200 25 1320 88 1440 68
+        L1440 120
+        L0 120
+        Z
+      "
+            fill="#FFF1D1"
+            opacity="0.55"
+          />
+        </svg>
 
-          <span>100% Safety Certified</span>
-          <span>•</span>
+        {/* CONTENT */}
+        <div className="relative top-8 z-10 flex h-full items-center justify-center px-8">
 
-          <span>5 Million+ Happy Visitors</span>
-          <span>•</span>
+          <div
+            className="
+        flex
+        items-center
+        gap-5
+        whitespace-nowrap
+        font-['Quicksand']
+        text-[18px]
+        font-semibold
+        tracking-[-0.01em]
+        text-[#4A3016]
+      "
+          >
+            <span>5 Million+ Happy Visitors</span>
+            <span>•</span>
 
-          <span>5 Million+ Happy Customer</span>
-          <span>•</span>
+            <span>5 Million+ Happy Customer</span>
+            <span>•</span>
 
-          <span>100% Safety Certified</span>
+            <span>100% Safety Certified</span>
+            <span>•</span>
+
+            <span>5 Million+ Happy Visitors</span>
+            <span>•</span>
+
+            <span>5 Million+ Happy Customer</span>
+            <span>•</span>
+
+            <span>100% Safety Certified</span>
+          </div>
         </div>
       </div>
     </section>
